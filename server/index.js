4ec3f0ba07032bc7f3e79005f600e5733cfb6df7
SIRI M M <sirimm2003@gmail.com>
2024-06-20 12:25:43 +0530

    update URI

--- server/index.js
+++ server/index.js
@@ -1,50 +1,52 @@
-const express= require('express')
-const mongoose= require('mongoose')
-const cors= require('cors')
-const TodoModel=require('./Models/Todo')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const TodoModel = require('./Models/Todo');
+require('dotenv').config();
 
-const app= express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
 
-app.get('/get',(req,res) =>{
+app.get('/get', (req, res) => {
     TodoModel.find()
-    .then(result => res.json(result))
-    .catch(err => res.log(err))
-})
+        .then(result => res.json(result))
+        .catch(err => console.log(err));
+});
 
-app.post('/add',(req,res) =>{
-    const task=req.body.task;
-    TodoModel.create({
-        task:task
-    }).then(result=>res.json(result))
-    .catch(err=>res.json(err))
-})
-
-app.put('/update/:id',(req,res)=>{
-    const {id} =  req.params;
-    TodoModel.findByIdAndUpdate({_id: id},{done:true})
-    .then(result => res.json(reslt))
-    .catch(err => res.json(err))
-})
-
-app.delete('/delete/:id',(req,res)=>{
-    const {id} =  req.params;
-    TodoModel.findByIdAndDelete({_id: id},{done:true})
-    .then(result => res.json(reslt))
-    .catch(err => res.json(err))
-})
-
-app.get('/search', (req, res) => {
-    const { query } = req.query;
-    TodoModel.find({ task: { $regex: query, $options: 'i' } }) 
+app.post('/add', (req, res) => {
+    const { task } = req.body;
+    TodoModel.create({ task })
         .then(result => res.json(result))
         .catch(err => res.json(err));
 });
 
+app.put('/update/:id', (req, res) => {
+    const { id } = req.params;
+    TodoModel.findByIdAndUpdate({ _id: id }, { done: true })
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
+});
 
-app.listen(3001,()=>{
-    console.log('Server is running on port 3001')
-})
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    TodoModel.findByIdAndDelete({ _id: id })
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
+});
+
+app.get('/search', (req, res) => {
+    const { query } = req.query;
+    TodoModel.find({ task: { $regex: query, $options: 'i' } })
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
